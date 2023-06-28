65a3a4b820b183bfcf0083a31d52e12c8a8b5614
Rhys <rhys@therhys.co.uk>
2023-06-28 17:19:45 +0100

    Remove redundant pages, fix robots.txt.

--- containers/webserver/html/mcrae/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'info.php';
-if($CLOSED){
-	echo "<center><b><h1>McRae Is Closed For Maintainence</h1></b></center><br>";
-	echo "<center><img src='images/pain.png'></center>";
-}else{
-	header('Location: main.php');
-}
-?>
