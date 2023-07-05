8f8f69265e4b2ca611d51955a0682935b2b76e1e
Rhys Adams <rhys@therhys.co.uk>
2023-07-05 09:47:54 +0200

    Separate post and blog list pages.

--- containers/webserver/html/blog.pl
+++ containers/webserver/html/blog.pl
@@ -49,86 +49,6 @@ sub posts {
       @out;    # Return sorted by publish date.
 }
 
-sub linkify_imgs {
-    my ($data) = @_;
-
-    $data =~ s:<img.*?src="(.*?)".*?>:<a href="$1">$&</a>:mg;
-
-    $data;
-}
-
-sub md2html {
-    my $out = markdown(
-        $_[0],
-        {
-            empty_element_suffix => '>',
-            tab_width            => 2
-        }
-    );
-
-    $out =~ s/<code>/<code class="prettyprint">/g;
-    $out = &linkify_imgs($out);
-
-    $out,
-      script({
-            src =>
-'https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js',
-        },
-        ''
-      );
-}
-
-sub load_post {
-    my ($fname) = @_;
-
-    $fname =~ s:/::g;    # Be safe and remove any slashes whatsoever.
-
-    $fname = "$POST_DIR/$fname";
-
-    open FH, '<', $fname or return h1("Unknown post!");
-
-    my %conf;
-    until ((my $line = <FH>) =~ /^----*/) {
-        next unless $line =~ /.*:.*/g;    # Skip unless there is key: value
-
-        my ($k, $v) = split /:\s*/, $line;
-        $conf{$k} = $v;
-    }
-    $conf{Tags} = [split ',\s*', $conf{Tags}];
-    my $data = join '', <FH>;
-    close FH;
-
-    my $infobar = div({
-            class => 'post_infobar'
-        },
-        p("Published: $conf{Published}"),
-        "Tags: ",
-        (
-            map {
-                a({ class => 'topic_round', href => "/blog.pl?tags=$_", }, $_)
-            } @{ $conf{Tags} }
-        ),
-    );
-
-    if ($fname =~ /\.md$/) {
-        return div({
-                class => 'post_body centre_page',
-            },
-            $infobar,
-            &md2html($data),
-        );
-    } elsif ($fname =~ /\.html/) {
-        return $infobar,
-          div({
-                class => 'post_body centre_page',
-            },
-            &linkify_imgs($data),
-          );
-    }
-
-    return h1("Unknown post type!");
-}
-
 sub page {
     (
         div({
@@ -145,7 +65,7 @@ sub page {
                 map {
                     li(
                         a({
-                                href => "/blog.pl?post=$$_[0]",
+                                href => "/post.pl?post=$$_[0]",
                             },
                             "$$_[1]{Title} - $$_[1]{Published}",
                         ),
@@ -168,18 +88,5 @@ sub index_body {
     div({ id => 'container' }, &navbar, &page, &footer,);
 }
 
-sub post_body {
-    my ($post_name) = @_;
-
-    div({ id => 'container' }, &navbar, &load_post($post_name), &footer,);
-}
-
-if (my $post_name = param('post')) {
-    print html (
-        CGI::head(&page_head('Rhys Adams - ' . param('post'))),
-        CGI::body(&post_body($post_name)),
-    );
-} else {
-    print html (CGI::head(&page_head('Rhys Adams - Blog')),
-        CGI::body(&index_body),);
-}
+print html (CGI::head(&page_head('Rhys Adams - Blog')),
+    CGI::body(&index_body),);
